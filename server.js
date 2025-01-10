819575d571c6ef1fd6d0f966e2a2db3533738a8f
Mohamed-Elsamouly <124587596+Mohamed-Elsamouly@users.noreply.github.com>
2025-01-10 21:27:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -45,7 +45,9 @@ io.on('connection', (socket) => {
 
         // If the session has 4 players, start the game
         if (session.players.length === 4) {
-            io.emit("find", { connected: true, sessionId: session.sessionId });
+            //io.emit("find", { connected: true, sessionId: session.sessionId });
+            session.players.forEach(player => {
+                io.to(player.socketId).emit("find", { connected: true, sessionId: session.sessionId });
             console.log("Players connected in session:", session.players.map(player => player.name));
         }
     });
